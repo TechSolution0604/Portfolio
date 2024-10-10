66505e29711a81b356bcb3996ce398ff616cd64d
Story-Teller-Coder <be.verify@outlook.com>
2024-10-10 19:41:33 +0900

    all files

---
+++ src/data/projectData.js
@@ -0,0 +1,124 @@
+export const projectData = [
+  {
+    id: crypto.randomUUID(),
+    title: 'Real estate management system',
+    description:
+      'With the aim of improving the efficiency of real estate sales and customer response, I was involved in a function expansion project for a system that centrally manages property information and customer information and provides the latest information on a website. I participated from the detailed design stage and played an important role in leading the requirements definition, design, testing, etc.',
+    technologies: ['Gin', 'React', 'TailwindCSS', 'MySQL', 'AWS RDS'],
+    imageUrl: '/rasters/1.png',
+    mobileUrl: '/rasters/1.png',
+    repository: 'https://github.com/Mully7773/FF-Search',
+    livelink: 'https://www.livemax.net/sale-real-estate/',
+  },
+  {
+    id: crypto.randomUUID(),
+    title: 'Hotel management and reservation management system',
+    description:
+      "To understand the actual situation at hotels, we visited several hotels and interviewed employees. From the perspective of UI/UX, we understood the actual work flow and aimed to design a system that was easy to use. We also focused on expanding the reservation function of the smartphone app, placing importance on expanding functions to meet customer needs. After the release, we regularly collected feedback and continued to update the system in an effort to maintain and improve customer satisfaction.",
+    technologies: ['Laravel', 'Vue', 'PostgreSQL', 'AWS S3', 'Nginx'],
+    imageUrl: 'rasters/2.png',
+    mobileUrl: 'rasters/2.png',
+    repository: 'https://github.com/Mully7773/Task-Manager',
+    livelink: 'https://www.hotel-livemax.com/',
+  },
+  {
+    id: crypto.randomUUID(),
+    title: 'Participate in the development of e-commerce services',
+    description:
+      "I optimized remote development to enable smooth collaboration between team members in different locations. Furthermore, I was always mindful of performance and security, and built a system that could withstand large amounts of traffic and unauthorized access. After the release, I was also involved in operational monitoring and maintenance.",
+    technologies: ['Gin', 'Alpine.js', 'Tailwind CSS', 'Nginx', 'Cloudflare'],
+    imageUrl: 'rasters/3.png',
+    mobileUrl: 'rasters/3.png',
+    repository: 'https://github.com/Mully7773/RPSLS',
+    livelink: 'https://www.kirin.co.jp/',
+  },
+  {
+    id: crypto.randomUUID(),
+    title: 'Used construction machinery sales System development',
+    description:
+      'I visited multiple construction companies and interviewed their employees. I understood user needs, such as how to search for used machinery and desired specifications, and reflected them in the design. I also visualized the existing manual sales flow, identified points for improving work efficiency, and incorporated them into the system requirements.',
+    technologies: ['Laravel', 'Angular', 'GSAP', 'Azure', 'FancyBox'],
+    imageUrl: 'rasters/4.png',
+    mobileUrl: 'rasters/4.png',
+    repository: 'https://github.com/Mully7773/Calculator',
+    livelink: 'https://kubotakenki.co.jp/',
+  },
+  {
+    id: crypto.randomUUID(),
+    title: 'New development of matching services',
+    description:
+      'From the start of the project, I clearly defined the target users of the service and focused on designing the service with that persona in mind.',
+    technologies: ['Ruby on Rails', 'Vue.js', 'AWS Lambda', 'AWS APIGateway', 'Chart.js'],
+    imageUrl: 'rasters/5.png',
+    mobileUrl: 'rasters/5.png',
+    repository: 'https://github.com/Mully7773/FiberChart',
+    livelink: 'https://www.value-press.com/',
+  },
+  {
+    id: crypto.randomUUID(),
+    title: 'Adding and improving functions for the real estate introduction system',
+    description:
+      "I set priorities by clarifying the background of each feature addition, such as the AI ​​chatbot to make up for labor shortages. I also focused on UI/UX design, pursuing simple and intuitive operability. Furthermore, we focused on multilingual support with an eye toward global expansion, and on strengthening security.",
+    technologies: [
+      'FastAPI',
+      'Angular',
+      'FireBase',
+      'Google Cloud',
+      'Docker'
+    ],
+    imageUrl: 'rasters/6.png',
+    mobileUrl: 'rasters/6.png',
+    repository: 'https://github.com/Mully7773/Huddle',
+    livelink: 'https://www.meiwa.jp/',
+  },
+  {
+    id: crypto.randomUUID(),
+    title: 'Sales management and logistics system improvement project',
+    description:
+      'In this project, the relationship of trust with the user company was beginning to deteriorate from the beginning. Therefore, I acted as a coordinator between the client`s various departments, thoroughly communicating with them and focusing on vendor control, while paying close attention to building relationships. Through this project, I was reminded of the importance of smooth communication with clients and the ability to execute projects.',
+    technologies: [
+      'Laravel',
+      'Next.js',
+      'AWS EC2',
+      'Apache2',
+      'Google Analytics',
+    ],
+    imageUrl: 'rasters/7.png',
+    mobileUrl: 'rasters/7.png',
+    repository: 'https://github.com/Mully7773/Advice-Generator',
+    livelink: 'https://www.astyle.jp/disp/CSfSpeDispListPage.jsp?tx=press160&dispNo=021001176',
+  },
+  {
+    id: crypto.randomUUID(),
+    title: 'Kitten sales support site Pet development',
+    description:
+      'I am building and operating the website "Pet no Jitsuya" that supports the sale of puppies and kittens by reputable breeders. It implements functions such as breeder registration, disclosure of pedigree and health information of puppies and kittens, and matching with potential buyers. In addition, I also provide an intermediary service for genetic testing of dogs and cats, and have developed a system for linking with testing institutions and an online application function for testing.',
+    technologies: ['Gin', 'React', 'AWS S3', 'MySQL', 'DataDog'],
+    imageUrl: 'rasters/8.png',
+    mobileUrl: 'rasters/8.png',
+    repository: 'https://github.com/Mully7773/Etch-A-Glow',
+    livelink: 'https://petzik-breeder.co.jp/',
+  },
+  {
+    id: crypto.randomUUID(),
+    title: 'Shopping mall system development-E-commerce',
+    description:
+      'It was a system that required high performance in the sense that it aggregated data from each tenant company and processed it in real time. Initially, performance was poor, and while investigating the cause, it was discovered that there was a problem with the database. A redesign contributed to improving performance.',
+    technologies: ['Django', 'Vue', 'MySQL', 'IOS', 'LineAPI'],
+    imageUrl: '/rasters/9.png',
+    mobileUrl: '/rasters/9.png',
+    repository: 'https://github.com/Mully7773/AdventureMap',
+    livelink: 'https://cucu.media/',
+  },
+  {
+    id: crypto.randomUUID(),
+    title: 'Development of Healthengine app for individual business owners',
+    description:
+      "I was involved in the entire process from planning to design and development of Healthengine, a smartphone app that supports health promotion. Its main functions include recording daily activity and food intake, setting goals and checking progress, and health advice using AI. By visualizing data and incorporating gamification elements, I have devised a way to help people develop healthy habits while having fun.",
+    technologies: ['Gin', 'Angular', 'MySQL', 'AWS EC2', 'Stripe'],
+    imageUrl: 'rasters/10.png',
+    mobileUrl: 'rasters/10.png',
+    repository: 'https://github.com/Mully7773/Nexter',
+    livelink: 'https://about.healthengine.com.au/',
+  },
+];
---
+++ src/data/skillData.js
@@ -0,0 +1,70 @@
+export const skillData = [
+  {
+    id: crypto.randomUUID(),
+    technology: 'Python',
+  },
+  {
+    id: crypto.randomUUID(),
+    technology: 'Go',
+  },
+  {
+    id: crypto.randomUUID(),
+    technology: 'Ruby',
+  },
+  {
+    id: crypto.randomUUID(),
+    technology: 'PHP',
+  },
+  {
+    id: crypto.randomUUID(),
+    technology: 'JavaScript/TypeScript',
+  },
+  {
+    id: crypto.randomUUID(),
+    technology: 'C#',
+  },
+  {
+    id: crypto.randomUUID(),
+    technology: 'Django',
+  },
+  {
+    id: crypto.randomUUID(),
+    technology: 'Gin',
+  },
+  {
+    id: crypto.randomUUID(),
+    technology: 'Ruby on Rails',
+  },
+  {
+    id: crypto.randomUUID(),
+    technology: 'Laravel',
+  },
+  {
+    id: crypto.randomUUID(),
+    technology: 'React',
+  },
+  {
+    id: crypto.randomUUID(),
+    technology: '.Net',
+  },
+  {
+    id: crypto.randomUUID(),
+    technology: 'MySQL',
+  },
+  {
+    id: crypto.randomUUID(),
+    technology: 'MongoDB',
+  },
+  {
+    id: crypto.randomUUID(),
+    technology: 'Postgre',
+  },
+  {
+    id: crypto.randomUUID(),
+    technology: 'ArangoDB',
+  },
+  {
+    id: crypto.randomUUID(),
+    technology: 'Redis',
+  },
+];
