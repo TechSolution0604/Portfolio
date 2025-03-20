b98f26e1f1cf331684b882e92d73e12195c1384a
TechSolution <126979325+TechSolution0923@users.noreply.github.com>
2025-03-21 01:33:55 +0900

    change the title

--- src/data/projectData.js
+++ src/data/projectData.js
@@ -1,7 +1,7 @@
 export const projectData = [
   {
     id: crypto.randomUUID(),
-    title: 'Hospital Management System',
+    title: '病院管理システム開発',
     description:
       'With the aim of improving the efficiency of real estate sales and customer response, I was involved in a function expansion project for a system that centrally manages property information and customer information and provides the latest information on a website. I participated from the detailed design stage and played an important role in leading the requirements definition, design, testing, etc.',
     technologies: ['Ruby on Rails', 'React', 'TailwindCSS', 'MySQL', 'AWS RDS'],
@@ -12,7 +12,7 @@ export const projectData = [
   },
   {
     id: crypto.randomUUID(),
-    title: 'Law Management System',
+    title: '法律管理システム開発',
     description:
       "To understand the actual situation at hotels, we visited several hotels and interviewed employees. From the perspective of UI/UX, we understood the actual work flow and aimed to design a system that was easy to use. We also focused on expanding the reservation function of the smartphone app, placing importance on expanding functions to meet customer needs. After the release, we regularly collected feedback and continued to update the system in an effort to maintain and improve customer satisfaction.",
     technologies: ['Gin', 'Angular', 'MongoDB', 'ArrangoDB', 'Nginx'],
@@ -23,7 +23,7 @@ export const projectData = [
   },
   {
     id: crypto.randomUUID(),
-    title: 'Hotel Management System',
+    title: 'ホテル管理システム開発',
     description:
       "I optimized remote development to enable smooth collaboration between team members in different locations. Furthermore, I was always mindful of performance and security, and built a system that could withstand large amounts of traffic and unauthorized access. After the release, I was also involved in operational monitoring and maintenance.",
     technologies: ['Laravel', 'Vue', 'MySQL', 'Notion', 'AWS EC2'],
@@ -34,7 +34,7 @@ export const projectData = [
   },
   {
     id: crypto.randomUUID(),
-    title: 'Crime Prevention App',
+    title: '犯罪対策アプリ開発',
     description:
       'I visited multiple construction companies and interviewed their employees. I understood user needs, such as how to search for used machinery and desired specifications, and reflected them in the design. I also visualized the existing manual sales flow, identified points for improving work efficiency, and incorporated them into the system requirements.',
     technologies: ['Django', 'OpenAI', 'Gemini', 'Langchain', 'AWS EC2'],
@@ -45,7 +45,7 @@ export const projectData = [
   },
   {
     id: crypto.randomUUID(),
-    title: 'New development of matching services',
+    title: 'マッチングシステム開発',
     description:
       'As a backend engineer, where I was responsible for the development of a matching system. In this project, I leveraged cutting-edge technologies such as FastAPI, OpenAI, and Claude 3 to build a system that optimizes the matching of job seekers and companies. I was particularly involved in designing and implementing AI-driven matching algorithms, using data analysis and machine learning models to deliver highly accurate matching functionality. Through this experience, I overcame technical challenges and learned how to maximize system performance.',
     technologies: ['FastAPI', 'OpenAI', 'Langchain', 'Google Cloud Vision', 'AWS Lambda'],
@@ -56,7 +56,7 @@ export const projectData = [
   },
   {
     id: crypto.randomUUID(),
-    title: 'Adding and improving functions for the real estate introduction system',
+    title: '不動産紹介システム開発',
     description:
       "I set priorities by clarifying the background of each feature addition, such as the AI ​​chatbot to make up for labor shortages. I also focused on UI/UX design, pursuing simple and intuitive operability. Furthermore, we focused on multilingual support with an eye toward global expansion, and on strengthening security.",
     technologies: [
@@ -73,7 +73,7 @@ export const projectData = [
   },
   {
     id: crypto.randomUUID(),
-    title: 'Sales management and logistics system improvement project',
+    title: '販売管理・物流システム改善',
     description:
       'In this project, the relationship of trust with the user company was beginning to deteriorate from the beginning. Therefore, I acted as a coordinator between the client`s various departments, thoroughly communicating with them and focusing on vendor control, while paying close attention to building relationships. Through this project, I was reminded of the importance of smooth communication with clients and the ability to execute projects.',
     technologies: [
@@ -90,7 +90,7 @@ export const projectData = [
   },
   {
     id: crypto.randomUUID(),
-    title: 'Kitten sales support site Pet development',
+    title: '子猫販売支援サイト',
     description:
       'I am building and operating the website "Pet no Jitsuya" that supports the sale of puppies and kittens by reputable breeders. It implements functions such as breeder registration, disclosure of pedigree and health information of puppies and kittens, and matching with potential buyers. In addition, I also provide an intermediary service for genetic testing of dogs and cats, and have developed a system for linking with testing institutions and an online application function for testing.',
     technologies: ['Gin', 'React', 'AWS S3', 'MySQL', 'DataDog'],
@@ -101,7 +101,7 @@ export const projectData = [
   },
   {
     id: crypto.randomUUID(),
-    title: 'Shopping mall system development-E-commerce',
+    title: 'ショッピングモールシステム開発',
     description:
       'It was a system that required high performance in the sense that it aggregated data from each tenant company and processed it in real time. Initially, performance was poor, and while investigating the cause, it was discovered that there was a problem with the database. A redesign contributed to improving performance.',
     technologies: ['Django', 'Vue', 'MySQL', 'IOS', 'LineAPI'],
@@ -112,7 +112,7 @@ export const projectData = [
   },
   {
     id: crypto.randomUUID(),
-    title: 'Development of Healthengine app for individual business owners',
+    title: 'Healthengineアプリの開発',
     description:
       "I was involved in the entire process from planning to design and development of Healthengine, a smartphone app that supports health promotion. Its main functions include recording daily activity and food intake, setting goals and checking progress, and health advice using AI. By visualizing data and incorporating gamification elements, I have devised a way to help people develop healthy habits while having fun.",
     technologies: ['Gin', 'Angular', 'MySQL', 'AWS EC2', 'Stripe'],
